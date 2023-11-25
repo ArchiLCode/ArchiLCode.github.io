259c50c8ac31c79bcb0f570945106168372df15e
ArchiLCode <119133282+ArchiLCode@users.noreply.github.com>
2023-11-26 01:14:07 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -26,3 +26,14 @@ burgerToggle.addEventListener("click", () => {
 window.onload = () => {
   main.style.opacity = "1";
 };
+
+new Swiper(".image-slider", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
