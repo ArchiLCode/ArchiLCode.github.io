fbbc8408ca00f0e62073dfa1a26d16c6a9385a6d
ArchiLCode <119133282+ArchiLCode@users.noreply.github.com>
2023-11-26 20:00:13 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,32 +3,40 @@ const burgerNavigation = document.getElementById("burger-nav");
 const burgerToggleTop = document.getElementById("toggle-line-top");
 const burgerToggleCenter = document.getElementById("toggle-line-center");
 const burgerToggleBottom = document.getElementById("toggle-line-bottom");
-const main = document.getElementById("main");
 
+// Состояние бургера
 var burgerOpened = false;
-burgerToggle.addEventListener("click", () => {
-  if (burgerOpened === false) {
-    burgerNavigation.style.display = "block";
-    burgerOpened = true;
-    burgerToggleCenter.style.visibility = "hidden";
-    burgerToggleTop.style.transform = "translateY(10px) rotate(45deg)";
-    burgerToggleBottom.style.transform = "translateY(-10px) rotate(-45deg)";
-    burgerNavigation.style.animation = "changeHeight 1s";
-    burgerNavigation.style.height = "calc(100vh - 80px)";
-    document.body.style.overflow = "hidden";
-    return;
-  }
-  burgerNavigation.style.display = "none";
+
+function openBurger() {
+  burgerOpened = true;
+  burgerToggleCenter.style.visibility = "hidden";
+  burgerToggleTop.style.transform = "translateY(10px) rotate(-45deg)";
+  burgerToggleBottom.style.transform = "translateY(-10px) rotate(45deg)";
+  burgerNavigation.style.height = "256.7px";
+}
+
+function closeBurger() {
+  burgerOpened = false;
   burgerToggleTop.style.transform = "translateY(0px) rotate(0deg)";
   burgerToggleBottom.style.transform = "translateY(0px) rotate(0deg)";
   burgerToggleCenter.style.visibility = "visible";
   burgerNavigation.style.height = "0";
-  document.body.style.overflow = "visible";
-  burgerOpened = false;
+}
+
+// Открытие и закрытие бургера по клику
+burgerToggle.addEventListener("click", () => {
+  if (burgerOpened === false) {
+    openBurger();
+    return;
+  }
+  closeBurger();
 });
 
 window.onload = () => {
+  // Плавное появление контента в #main
   main.style.opacity = "1";
+
+  // Инициализация свайпера
   new Swiper(".image-slider", {
     navigation: {
       nextEl: ".swiper-button-next",
@@ -38,5 +46,9 @@ window.onload = () => {
       el: ".swiper-pagination",
       clickable: true,
     },
+    spaceBetween: 5,
+    preloadImages: false,
+
+    lazyPreloadPrevNext: 1,
   });
 };
